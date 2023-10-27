fe79fbbb743a6d291531c7d7a00f13d2ddf7fa9d
Shivan Sivakumaran <129010893+shivan-eyespace@users.noreply.github.com>
2023-10-27 15:21:47 +1300

    adding db bindings

--- lua/custom/plugins/dadbod.lua
+++ lua/custom/plugins/dadbod.lua
@@ -3,5 +3,8 @@ return {
 	dependencies = {
 		"tpope/vim-dadbod",
 		'kristijanhusak/vim-dadbod-completion'
-	}
+	},
+	config = function()
+		vim.keymap.set('n', '<leader>db', "<cmd>DBUIToggle<CR>", { expr = true, silent = true, desc = "[DB]UI" })
+	end
 }
