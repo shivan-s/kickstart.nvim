774d9f77d50f89224e5a09a15f40cf54ab88acc4
Shivan Sivakumaran <129010893+shivan-eyespace@users.noreply.github.com>
2024-09-04 14:26:02 +1200

    chore: improves

--- init.lua
+++ init.lua
@@ -134,7 +134,6 @@ require('lazy').setup({
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
-      -- See `:help gitsigns.txt`
       signs = {
         add = { text = '+' },
         change = { text = '~' },
