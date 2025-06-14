4c4cfcffd141a051bbbb6bb7f08232c33b7472ad
Shivan Sivakumaran <me@shivan.xyz>
2025-06-14 22:05:40 +1200

    chore: improve nvim tree

--- lua/custom/plugins/nvim-tree.lua
+++ lua/custom/plugins/nvim-tree.lua
@@ -3,84 +3,106 @@
 -- https://github.com/nvim-tree/nvim-tree.lua
 -- ==========================================
 vim.g.nvim_tree_icons = {
-	default = "",
-	symlink = "",
-	git = {
-		unstaged = "",
-		staged = "S",
-		unmerged = "",
-		renamed = "➜",
-		deleted = "",
-		untracked = "U",
-		ignored = "◌",
-	},
-	folder = {
-		default = "",
-		open = "",
-		empty = "",
-		empty_open = "",
-		symlink = "",
-	},
+  default = '',
+  symlink = '',
+  git = {
+    unstaged = '',
+    staged = 'S',
+    unmerged = '',
+    renamed = '➜',
+    deleted = '',
+    untracked = 'U',
+    ignored = '◌',
+  },
+  folder = {
+    default = '',
+    open = '',
+    empty = '',
+    empty_open = '',
+    symlink = '',
+  },
 }
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 vim.opt.termguicolors = true
 
-vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeToggle<CR>",
-	{ noremap = true, silent = true, desc = "Nvimtree Toggle" })
+vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<CR>', { noremap = true, silent = true, desc = 'Nvimtree Toggle' })
+
+local HEIGHT_RATIO = 0.8
+local WIDTH_RATIO = 0.5
 
 return {
-	"nvim-tree/nvim-tree.lua",
-	version = "*",
-	dependencies = {
-		"nvim-tree/nvim-web-devicons",
-	},
-	config = function()
-		require('nvim-tree').setup {
-			diagnostics = {
-				enable = true,
-				icons = {
-					hint = "",
-					info = "",
-					warning = "",
-					error = "",
-				},
-			},
-			update_focused_file = {
-				enable = true,
-				update_cwd = true,
-				ignore_list = {},
-			},
-			system_open = {
-				cmd = nil,
-				args = {},
-			},
-			filters = {
-				dotfiles = false,
-			},
-			git = {
-				enable = true,
-				ignore = true,
-				timeout = 500,
-			},
-			view = {
-				width = 40,
-				side = "left",
-				number = false,
-				relativenumber = false,
-			},
-			trash = {
-				cmd = "trash",
-				require_confirm = true,
-			},
-			actions = {
-				open_file = {
-					quit_on_open = true,
-					window_picker = {
-						enable = false,
-					},
-				},
-			},
-		}
-	end
+  'nvim-tree/nvim-tree.lua',
+  version = '*',
+  dependencies = {
+    'nvim-tree/nvim-web-devicons',
+  },
+  config = function()
+    require('nvim-tree').setup {
+      diagnostics = {
+        enable = true,
+        icons = {
+          hint = '',
+          info = '',
+          warning = '',
+          error = '',
+        },
+      },
+      update_focused_file = {
+        enable = true,
+        update_cwd = true,
+        ignore_list = {},
+      },
+      system_open = {
+        cmd = nil,
+        args = {},
+      },
+      filters = {
+        dotfiles = false,
+      },
+      git = {
+        enable = true,
+        ignore = true,
+        timeout = 500,
+      },
+      view = {
+        float = {
+          enable = true,
+          open_win_config = function()
+            local screen_w = vim.opt.columns:get()
+            local screen_h = vim.opt.lines:get() - vim.opt.cmdheight:get()
+            local window_w = screen_w * WIDTH_RATIO
+            local window_h = screen_h * HEIGHT_RATIO
+            local window_w_int = math.floor(window_w)
+            local window_h_int = math.floor(window_h)
+            local center_x = (screen_w - window_w) / 2
+            local center_y = ((vim.opt.lines:get() - window_h) / 2) - vim.opt.cmdheight:get()
+            return {
+              border = 'rounded',
+              relative = 'editor',
+              row = center_y,
+              col = center_x,
+              width = window_w_int,
+              height = window_h_int,
+            }
+          end,
+        },
+        width = function()
+          return math.floor(vim.opt.columns:get() * WIDTH_RATIO)
+        end,
+      },
+      trash = {
+        cmd = 'trash',
+        require_confirm = true,
+      },
+      actions = {
+        open_file = {
+          quit_on_open = true,
+          window_picker = {
+            enable = false,
+          },
+        },
+      },
+    }
+  end,
 }
