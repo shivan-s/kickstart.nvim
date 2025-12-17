2cf20e2279694e2b50a8cc9f0ba2adec4fcb5fbb
Shivan Sivakumaran <129010893+shivan-eyespace@users.noreply.github.com>
2025-12-18 10:38:22 +1300

    chore: remove dart

--- lua/custom/plugins/none-ls.lua
+++ lua/custom/plugins/none-ls.lua
@@ -1,26 +1,25 @@
 return {
-	"nvimtools/none-ls.nvim",
-	dependences = "plenary.nvim",
-	config = function()
-		local none_ls = require("null-ls")
-		local f = none_ls.builtins.formatting
-		local l = none_ls.builtins.diagnostics
-		none_ls.setup({
-			sources = {
-				-- Golang
-				f.gofmt,
-				f.golines,
-				f.goimports,
-				f.goimports_reviser,
-				f.sql_formatter,
-				f.prettierd,
-				l.codespell,
-				l.proselint,
-				f.dart_format,
-			},
-			should_attach = function(bufnr)
-				return not vim.api.nvim_buf_get_name(bufnr):match("NvimTree")
-			end
-		})
-	end
+  'nvimtools/none-ls.nvim',
+  dependences = 'plenary.nvim',
+  config = function()
+    local none_ls = require 'null-ls'
+    local f = none_ls.builtins.formatting
+    local l = none_ls.builtins.diagnostics
+    none_ls.setup {
+      sources = {
+        -- Golang
+        f.gofmt,
+        f.golines,
+        f.goimports,
+        f.goimports_reviser,
+        f.sql_formatter,
+        f.prettierd,
+        l.codespell,
+        l.proselint,
+      },
+      should_attach = function(bufnr)
+        return not vim.api.nvim_buf_get_name(bufnr):match 'NvimTree'
+      end,
+    }
+  end,
 }
