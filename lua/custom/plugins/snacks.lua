d50f66d87dd551ba89d89718557f83461fe520c0
Shivan Sivakumaran <me@shivan.xyz>
2026-01-21 07:13:47 +1300

    chore: remove header

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -11,7 +11,6 @@ return {
     dashboard = {
       enabled = true,
       sections = {
-        { section = 'header' },
         { icon = ' ', title = 'Keymaps', section = 'keys', indent = 2, padding = 1 },
         { icon = ' ', title = 'Recent Files', section = 'recent_files', indent = 2, padding = 1 },
         { icon = ' ', title = 'Projects', section = 'projects', indent = 2, padding = 1 },
