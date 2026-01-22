b66aa40e748e37cd4fa32c28cd67b0fb09c61f2d
Shivan Sivakumaran <me@shivan.xyz>
2026-01-23 11:33:06 +1300

    feat: add zk

---
+++ lua/custom/plugins/zk.lua
@@ -0,0 +1,18 @@
+return {
+  'zk-org/zk-nvim',
+  config = function()
+    require('zk').setup {
+      picker = 'telescope',
+      lsp = {
+        config = {
+          name = 'zk',
+          cmd = { 'zk', 'lsp' },
+          filetypes = { 'markdown' },
+        },
+        auto_attach = {
+          enabled = true,
+        },
+      },
+    }
+  end,
+}
